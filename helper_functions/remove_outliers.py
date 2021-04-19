c2de80c46d8a1b29cd6f3039d1ff81b873429975
capac <capac@users.noreply.github.com>
2021-04-19 21:07:07 +0100

    Added class to remove outliers with StandardScaler

--- helper_functions/remove_outliers.py
+++ helper_functions/remove_outliers.py
@@ -3,6 +3,7 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import StandardScaler
 
 
 class OutlierExtractor(BaseEstimator, TransformerMixin):
@@ -24,7 +25,7 @@ class OutlierExtractor(BaseEstimator, TransformerMixin):
         self.kwargs = kwargs
 
     def transform(self, X, y):
-        """
+        '''
         Uses LocalOutlierFactor class to subselect data based on some threshold
 
         Returns:
@@ -32,7 +33,7 @@ class OutlierExtractor(BaseEstimator, TransformerMixin):
 
         Notes:
             X should be of shape (n_samples, n_features)
-        """
+        '''
         X = np.asarray(X)
         y = np.asarray(y)
         lcf = LocalOutlierFactor(**self.kwargs)
@@ -42,3 +43,32 @@ class OutlierExtractor(BaseEstimator, TransformerMixin):
 
     def fit(self, *args, **kwargs):
         return self
+
+
+class RemoveMetricOutliers(BaseEstimator, TransformerMixin):
+    def __init__(self, **kwargs):
+        '''
+        Removes outliers in performance metrics by standardizing samples
+        using StandardScaler. A sigma threshold is set for outlier removal.
+
+        Keyword Args:
+            sigma (float): The threshold for excluding samples such that the absolute
+            value of the sample is less than the threshold sigma.
+
+        Returns:
+            ndarray: subsampled data
+        '''
+        self.threshold = kwargs.pop('sigma', 2.0)
+
+        self.kwargs = kwargs
+
+    def fit(self, *args, **kwargs):
+        return self
+
+    def transform(self, X, y):
+        X = np.asarray(X)
+        y = np.asarray(y).reshape(-1, 1)
+        ss = StandardScaler(**self.kwargs)
+        y_tr = ss.fit_transform(y)
+        return (X[(np.abs(y_tr) < self.threshold).reshape(X.shape[0])],
+                y[(np.abs(y_tr) < self.threshold).reshape(X.shape[0])])

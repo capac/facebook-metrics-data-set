d78d3ef64fa63df1f7e149ec612aedf9a41df891
capac <capac@users.noreply.github.com>
2022-07-26 18:04:29 +0100

    Removed class that used 'LocalOutlierFactor'

--- helper_functions/remove_outliers.py
+++ helper_functions/remove_outliers.py
@@ -2,50 +2,11 @@
 
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.neighbors import LocalOutlierFactor
 from sklearn.preprocessing import StandardScaler
 
 
-class OutlierExtractor(BaseEstimator, TransformerMixin):
-    def __init__(self, **kwargs):
-        '''
-        Create a transformer to remove outliers. A threshold is set for selection
-        criteria, and further arguments are passed to the LocalOutlierFactor class
-
-        Keyword Args:
-            neg_conf_val (float): The threshold for excluding samples with a lower
-               negative outlier factor.
-
-        Returns:
-            object: to be used as a transformer method as part of Pipeline()
-        '''
-
-        self.threshold = kwargs.pop('neg_conf_val', -10.0)
-
-        self.kwargs = kwargs
-
-    def transform(self, X, y):
-        '''
-        Uses LocalOutlierFactor class to subselect data based on some threshold
-
-        Returns:
-            ndarray: subsampled data
-
-        Notes:
-            X should be of shape (n_samples, n_features)
-        '''
-        X = np.asarray(X)
-        y = np.asarray(y)
-        lcf = LocalOutlierFactor(**self.kwargs)
-        lcf.fit(X)
-        return (X[lcf.negative_outlier_factor_ > self.threshold, :],
-                y[lcf.negative_outlier_factor_ > self.threshold])
-
-    def fit(self, *args, **kwargs):
-        return self
-
-
-class RemoveMetricOutliers(BaseEstimator, TransformerMixin):
+# https://stackoverflow.com/questions/52346725/can-i-add-outlier-detection-and-removal-to-scikit-learn-pipeline
+class RemoveOutliers(BaseEstimator, TransformerMixin):
     def __init__(self, **kwargs):
         '''
         Removes outliers in performance metrics by standardizing samples
@@ -60,11 +21,6 @@ class RemoveMetricOutliers(BaseEstimator, TransformerMixin):
         '''
         self.threshold = kwargs.pop('sigma', 2.0)
 
-        self.kwargs = kwargs
-
-    def fit(self, *args, **kwargs):
-        return self
-
     def transform(self, X, y):
         '''
         Uses StandardScaler to standardize y label values and retain only
@@ -74,7 +30,7 @@ class RemoveMetricOutliers(BaseEstimator, TransformerMixin):
             ndarray: subsampled data
         '''
         y = y.reshape(-1, 1)
-        ss = StandardScaler(**self.kwargs)
+        ss = StandardScaler()
         y_tr = ss.fit_transform(y)
-        return (X[(np.abs(y_tr) < self.threshold).reshape(X.shape[0])],
-                y[(np.abs(y_tr) < self.threshold).reshape(X.shape[0])])
+        return (X[(np.abs(y_tr) <= self.threshold).reshape(X.shape[0])],
+                y[(np.abs(y_tr) <= self.threshold).reshape(X.shape[0])])

279bfe2a521041a32af77ccd6a1e87b98685dec2
capac <capac@users.noreply.github.com>
2022-08-08 17:25:12 +0100

    Created 'DataModeling' class; threshold=2.0

--- modeling.py
+++ modeling.py
@@ -23,64 +23,73 @@ data_file = work_dir / 'data/dataset_Facebook.csv'
 data_prep = DataPreparation(data_file)
 fb_na_tr = data_prep.transform()
 
-# Comment on output columns:
-# With the OneHotEncoder class, the number of columns in the transformed matrix
-# is 57. The last 12 columns are the performance metrics which are used as labels
-# for modeling.
-
 
 # data modeling
-def cv_performance_model(model, threshold=3.0):
-    cross_val_scores = []
-    # 12 columns for preformance metrics
-    num_metrics = fb_na_tr.shape[1]-1
-    # range from 56 (included) to 45 (included)
-    for col in range(num_metrics, num_metrics-len(data_prep.output_columns), -1):
-        clone_model = clone(model)
-        # range from 0 (included) to 44 (included), all categorical except for the last one
-        X, y = fb_na_tr[:, 0:num_metrics-len(data_prep.output_columns)+1], fb_na_tr[:, col]
-        X_thr, y_thr = X[(np.abs(y) < threshold)], y[(np.abs(y) < threshold)]
-        scores = cross_val_score(clone_model, X_thr, y_thr, cv=5,
-                                 scoring='neg_root_mean_squared_error')
-        rmse_mean = -scores.mean()
-        rmse_std = scores.std()
-        err_perc = 100*rmse_std/rmse_mean
-        cross_val_scores.append({'rmse': round(rmse_mean, 6),
-                                 'std': round(rmse_std, 6),
-                                 'perc': round(err_perc, 6)})
-    return cross_val_scores
+class DataModeling():
+    '''
+    With the OneHotEncoder class, the number of columns in the transformed matrix is 57.
+    The last 12 columns are the performance metrics which are used as labels for modeling.
+    '''
+    def __init__(self, data, model, perf_metric_cols, threshold=2.0):
+        self.data = data
+        self.model = model
+        self.threshold = threshold
+        # 12 preformance metrics columns, from last to first
+        self.perf_metric_cols = perf_metric_cols[::-1]
+        # total number of columns
+        self.tot_num_cols = self.data.shape[1]
+        self.diff_cols = self.tot_num_cols - len(self.perf_metric_cols)
 
+    def _cal_perf_metrics(self):
+        cross_val_scores = []
+        # range from 56 (included) to 45 (included)
+        for col in range(self.tot_num_cols-1, self.diff_cols-1, -1):
+            # range from 0 (included) to 44 (included), all categorical except for the last one
+            X = self.data[:, 0:self.diff_cols].copy()
+            y = self.data[:, col].copy()
+            clone_model = clone(self.model)
+            # removing outliers
+            X_thr = X[(np.abs(y) < self.threshold)]
+            y_thr = y[(np.abs(y) < self.threshold)]
+            scores = cross_val_score(clone_model, X_thr, y_thr, cv=5,
+                                     scoring='neg_root_mean_squared_error')
+            rmse_mean = -scores.mean()
+            rmse_std = scores.std()
+            err_perc = 100*rmse_std/rmse_mean
+            cross_val_scores.append({'rmse': round(rmse_mean, 6),
+                                     'std': round(rmse_std, 6),
+                                     'perc': round(err_perc, 6)})
+        return cross_val_scores
 
-def performance_model_table(model):
-    t0 = time()
-    cross_val_scores = cv_performance_model(model)
-    data_prep.output_columns.reverse()
-    reg_df = pd.DataFrame(cross_val_scores, index=data_prep.output_columns)
-    reg_df.sort_values(by='rmse', ascending=True, inplace=True)
-    reg_df.reset_index(inplace=True)
-    reg_df.rename(columns={'index': 'Performance metric', 'rmse': 'RMSE',
-                           'std': 'SD (sigma)', 'perc': 'Error (%)'}, inplace=True)
-    print(f'Time elapsed for {model.__class__.__name__}: {round(time() - t0, 2)} s.')
-    return reg_df
+    def perf_table(self):
+        t0 = time()
+        cross_val_scores = self._cal_perf_metrics()
+        reg_df = pd.DataFrame(cross_val_scores, index=self.perf_metric_cols)
+        reg_df.sort_values(by='rmse', ascending=True, inplace=True)
+        reg_df.reset_index(inplace=True)
+        reg_df.rename(columns={'index': 'Performance metric', 'rmse': 'RMSE',
+                               'std': 'SD (sigma)', 'perc': 'Error (%)'}, inplace=True)
+        print(f'Time elapsed for {model.__class__.__name__}: {round(time() - t0, 2)} s.')
+        return reg_df
 
 
 # coef_ weights are only available with SVR(kernel='linear')
-model_list = {'Random Forest Regressor': RandomForestRegressor(n_estimators=200,
+model_list = {'Support Vector Machine Regressor': SVR(kernel='rbf', C=0.5),
+              'Random Forest Regressor': RandomForestRegressor(n_estimators=200,
                                                                random_state=42,
                                                                n_jobs=-1),
+              'Ridge': Ridge(random_state=42),
               'XGBRegressor': XGBRegressor(n_estimators=200,
                                            random_state=42,
                                            eval_metric='rmse',
                                            n_jobs=-1),
-              'Support Vector Machine Regressor': SVR(kernel='rbf', C=0.5),
-              'Ridge': Ridge(random_state=42),
               }
 
 # model calculation and saving output to file
 with open(work_dir / 'stats_output_no_outliers_in_metrics.txt', 'w') as f:
     t1 = time()
     for name, model in model_list.items():
-        results = performance_model_table(model)
-        f.writelines(f'Results for {name}: \n{(results)}\n\n')
+        data_metrics = DataModeling(fb_na_tr, model, data_prep.output_columns)
+        f.writelines(f'Results for {name}: \n{(data_metrics.perf_table())}\n\n')
     f.writelines('\n')
     print(f'Total time elapsed: {round(time() - t1, 2)} s.')

5bc1634ba3b211ed0e725527aab6165da75bdb95
capac <capac@users.noreply.github.com>
2022-12-07 13:17:33 +0000

    Added all performance metrics in standardized plot

--- plot_performance_metrics.py
+++ plot_performance_metrics.py
@@ -15,15 +15,22 @@ data_file = work_dir / 'data/dataset_Facebook.csv'
 # data preparation
 data_prep = DataPreparation(data_file)
 fb_df_tr = data_prep.transform()
+columns = ['Lifetime Post Total Reach', 'Lifetime Post Total Impressions',
+           'Lifetime Engaged Users', 'Lifetime Post Consumers',
+           'Lifetime Post Consumptions',
+           'Lifetime Post Impressions by people who have liked your Page',
+           'Lifetime Post reach by people who like your Page',
+           'Lifetime People who have liked your Page and engaged with your post',
+           'comment', 'like', 'share', 'Total Interactions']
 
 # plots
-fb_df_tr = pd.DataFrame(fb_df_tr[:, 7:15], columns=data_prep.output_columns)
-fig, axes = plt.subplots(nrows=4, ncols=2, figsize=(13, 7))
+fb_df_tr = pd.DataFrame(fb_df_tr[:, 7:19], columns=columns)
+fig, axes = plt.subplots(nrows=6, ncols=2, figsize=(13, 7))
 # using 'Paired' colormap
 cmap = plt.cm.Paired.colors
-for ax, clm, col in zip(axes.flat, data_prep.output_columns, cmap):
+for ax, clm, col in zip(axes.flat, columns, cmap):
     ax.hist(fb_df_tr.loc[:, clm], edgecolor='k', alpha=0.9, color=col)
-    ax.set_title(clm)
+    ax.set_title(clm.capitalize())
 plt.suptitle('Distribution of standardized performance metrics')
 plt.tight_layout()
 plt.savefig('plots/performance_metrics_hist.png', dpi=288, bbox_inches='tight')

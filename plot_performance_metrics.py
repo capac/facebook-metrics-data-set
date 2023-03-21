8e0a406bf51e681df3cc448d3fc68dfc10a7f300
capac <capac@users.noreply.github.com>
2023-03-21 12:00:18 +0000

    Updated plot_performance_metrics.py

--- plot_performance_metrics.py
+++ plot_performance_metrics.py
@@ -12,23 +12,15 @@ data_file = work_dir / 'data/dataset_Facebook.csv'
 
 # data preparation
 data_prep = DataPreparation(data_file)
-fb_df_tr = data_prep.transform()
-columns = ['Lifetime Post Total Reach', 'Lifetime Post Total Impressions',
-           'Lifetime Engaged Users', 'Lifetime Post Consumers',
-           'Lifetime Post Consumptions',
-           'Lifetime Post Impressions by people who have liked your Page',
-           'Lifetime Post reach by people who like your Page',
-           'Lifetime People who have liked your Page and engaged with your post',
-           'comment', 'like', 'share', 'Total Interactions']
+fb_na_tr = data_prep.transform()
 
 # plots
-fb_df_tr = pd.DataFrame(fb_df_tr[:, 7:19], columns=columns)
-fig, axes = plt.subplots(nrows=6, ncols=2, figsize=(13, 7))
+fb_mod_df = pd.DataFrame(fb_na_tr[0], columns=fb_na_tr[1])
+fig, axes = plt.subplots(nrows=13, ncols=1, figsize=(8, 18), layout='constrained')
 # using 'Paired' colormap
-cmap = plt.cm.Paired.colors
-for ax, clm, col in zip(axes.flat, columns, cmap):
-    ax.hist(fb_df_tr.loc[:, clm], edgecolor='k', alpha=0.9, color=col)
+cmap = plt.cm.tab20.colors
+for ax, clm, col in zip(axes.flat, fb_mod_df.columns.tolist()[-13:], cmap):
+    ax.hist(fb_mod_df[clm], edgecolor='k', alpha=0.9, color=col, bins=60,)
     ax.set_title(clm.capitalize())
-plt.suptitle('Distribution of standardized performance metrics')
-plt.tight_layout()
+plt.suptitle("Standardized performance metrics and 'Page total likes'")
 plt.savefig('plots/performance_metrics_hist.png', dpi=288, bbox_inches='tight')

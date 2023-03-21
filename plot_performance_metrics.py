20ec2b42f17e764aa0ae13dfa1ccae96c5caab39
capac <capac@users.noreply.github.com>
2023-03-21 16:14:49 +0000

    Performance metrics and 'Page total likes' in standardized and logarithmic standardized histograms

--- plot_performance_metrics.py
+++ plot_performance_metrics.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import pandas as pd
+import numpy as np
 # matplotlib
 import matplotlib.pyplot as plt
 from data_preparation import DataPreparation
@@ -14,13 +15,24 @@ data_file = work_dir / 'data/dataset_Facebook.csv'
 data_prep = DataPreparation(data_file)
 fb_na_tr = data_prep.transform()
 
-# plots
+# standardized performance metrics plot
 fb_mod_df = pd.DataFrame(fb_na_tr[0], columns=fb_na_tr[1])
 fig, axes = plt.subplots(nrows=13, ncols=1, figsize=(8, 18), layout='constrained')
-# using 'Paired' colormap
+# using 'tab20' colormap
 cmap = plt.cm.tab20.colors
 for ax, clm, col in zip(axes.flat, fb_mod_df.columns.tolist()[-13:], cmap):
     ax.hist(fb_mod_df[clm], edgecolor='k', alpha=0.9, color=col, bins=60,)
     ax.set_title(clm.capitalize())
 plt.suptitle("Standardized performance metrics and 'Page total likes'")
 plt.savefig('plots/performance_metrics_hist.png', dpi=288, bbox_inches='tight')
+
+
+# standardized logarithmic performance metrics plot
+fig, axes = plt.subplots(nrows=13, ncols=1, figsize=(8, 18), layout='constrained')
+# using 'tab20' colormap
+cmap = plt.cm.tab20.colors
+for ax, clm, col in zip(axes.flat, fb_mod_df.columns.tolist()[-13:], cmap):
+    ax.hist(np.log(fb_mod_df[clm]+1), edgecolor='k', alpha=0.9, color=col, bins=60,)
+    ax.set_title(clm.capitalize())
+plt.suptitle("Standardized logarithmic performance metrics and 'Page total likes'")
+plt.savefig('plots/performance_metrics_log_hist.png', dpi=288, bbox_inches='tight')

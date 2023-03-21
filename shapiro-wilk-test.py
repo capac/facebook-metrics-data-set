c6c1e4f19e89fad59d5e3a7163ccaf8ee0cf5642
capac <capac@users.noreply.github.com>
2023-03-21 16:02:59 +0000

    Standardized logarithmic values for performance columns for Shapiro-Wilk test

--- shapiro-wilk-test.py
+++ shapiro-wilk-test.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 from data_preparation import DataPreparation
 from scipy.stats import shapiro
+import pandas as pd
+import numpy as np
 
 home_dir = Path.home()
 work_dir = home_dir / 'Programming/Python/machine-learning-exercises/uci-ml-repository/facebook-metrics-data-set'
@@ -10,12 +12,14 @@ data_file = work_dir / 'data/dataset_Facebook.csv'
 
 # data preparation
 data_prep = DataPreparation(data_file)
-fb_df_tr = data_prep.transform()
+fb_na_tr = data_prep.transform()
+fb_mod_df = pd.DataFrame(fb_na_tr[0], columns=fb_na_tr[1])
 
 # test calculation and saving output to file
+# https://en.wikipedia.org/wiki/Shapiro%E2%80%93Wilk_test
 with open(work_dir / 'shapiro-wilk-test-results.txt', 'w') as f:
-    for col, name in zip(range(7, 15), data_prep.output_columns):
-        result = shapiro(fb_df_tr[:, col])
-        f.writelines(f'''Result for '{name}': \n{(result)}\n\n''')
+    for col_name in fb_na_tr[1].tolist()[-12:]:
+        result = shapiro(np.log(fb_mod_df[col_name]+1))
+        f.writelines(f'''Result for '{col_name.capitalize()}': \n{(result)}\n\n''')
     f.writelines('\n')
     print('Done!')

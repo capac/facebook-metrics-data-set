51314dceb8c16616838521400af00da9dcd9fcb4
capac <capac@users.noreply.github.com>
2023-03-22 16:07:06 +0000

    Updated for different performance metrics plot layout

--- plot_performance_metrics.py
+++ plot_performance_metrics.py
@@ -27,7 +27,7 @@ plt.suptitle("Standardized performance metrics and 'Page total likes'")
 plt.savefig('plots/performance_metrics_hist.png', dpi=288, bbox_inches='tight')
 
 
-# standardized logarithmic performance metrics plot
+# standardized logarithmic performance metrics and 'Page total likes'
 fig, axes = plt.subplots(nrows=13, ncols=1, figsize=(8, 18), layout='constrained')
 # using 'tab20' colormap
 cmap = plt.cm.tab20.colors
@@ -36,3 +36,14 @@ for ax, clm, col in zip(axes.flat, fb_mod_df.columns.tolist()[-13:], cmap):
     ax.set_title(clm.capitalize())
 plt.suptitle("Standardized logarithmic performance metrics and 'Page total likes'")
 plt.savefig('plots/performance_metrics_log_hist.png', dpi=288, bbox_inches='tight')
+
+
+# standardized logarithmic performance metrics plot and different layout
+fig, axes = plt.subplots(nrows=6, ncols=2, figsize=(12, 8), layout='constrained')
+# using 'tab20' colormap
+cmap = plt.cm.tab20.colors
+for ax, clm, col in zip(axes.flat, fb_mod_df.columns.tolist()[-12:], cmap):
+    ax.hist(np.log(fb_mod_df.loc[:, clm]+1), edgecolor='k', alpha=0.9, color=col, bins=60,)
+    ax.set_title(clm.capitalize())
+plt.suptitle("Logarithmic standardized performance metrics")
+plt.savefig('plots/performance_metrics_log_hist_2.png', dpi=288, bbox_inches='tight')
